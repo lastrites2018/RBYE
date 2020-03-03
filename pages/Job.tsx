75be0b92da1f94a6f59fd5b057ce5c67111ec008
lastrites2018 <lastrites@nate.com>
2020-03-03 23:42:32 +0900

    한글 변수명을 영어 변수명으로 교체하라

--- pages/Job.tsx
+++ pages/Job.tsx
@@ -1,16 +1,18 @@
 import Markdown from "react-markdown";
 
-const Jobs = ({ subject, companyName, contentObj, link, 마감일 }) => (
+const Jobs = ({ subject, companyName, contentObj, link, closingDate }) => (
   <div className="p-4 shadow rounded bg-white mt-2 sm:p-2 sm:m-2">
     <h1 className="text-green-500 text-3xl ">{companyName}</h1>
     <p className="text-gray-800">{subject}</p>
     <div className="text-red-800 sm:m-2 md:m-10">
-      <Markdown source={contentObj["자격요건"].replace(/• /gi, "- • ")} />
+      <Markdown source={contentObj.requirement.replace(/• /gi, "- • ")} />
     </div>
     <h6 className="sm:m-2 md:m-4">우대사항</h6>
-    <p className="text-gray-500 mb-2 sm:m-2 md:m-4">{contentObj["우대사항"]}</p>
+    <p className="text-gray-500 mb-2 sm:m-2 md:m-4">
+      {contentObj.preferentialTreatment}
+    </p>
     <h6 className="sm:m-2 md:m-4">주요업무</h6>
-    <p className="text-gray-600 mb-2 sm:m-2 md:m-4">{contentObj["주요업무"]}</p>
+    <p className="text-gray-600 mb-2 sm:m-2 md:m-4">{contentObj.mainTask}</p>
     <div className="text-right">
       <a
         className="text-blue-600 hover:text-blue-400"
@@ -19,7 +21,7 @@ const Jobs = ({ subject, companyName, contentObj, link, 마감일 }) => (
       >
         {link}
       </a>
-      <span className="ml-2 text-gray-500">마감일 {마감일}</span>
+      <span className="ml-2 text-gray-500">마감일 {closingDate}</span>
     </div>
   </div>
 );
