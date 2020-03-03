e8b82677d1c48d8dafd5c606b7ec21c06b1c5316
lastrites2018 <lastrites@nate.com>
2020-03-04 00:44:09 +0900

    임시 검색 기능을 추가하라

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,18 +21,59 @@ interface Job {
 
 const IndexPage = (props: any) => {
   // const IndexPage: NextPage = props => {
+  const [data, setData] = React.useState(props.data);
+  const [year, setYear] = React.useState(0);
+
+  React.useEffect(() => {
+    async function getData() {
+      const res = await fetch(
+        `https://rbye-api.lastrites.now.sh/table?q=${year}년`
+      );
+      const newData = await res.json();
+      console.log("newData: ", newData);
+      await setData(newData);
+    }
+
+    if (year === 0) {
+      return setData(props.data);
+    }
+
+    if (year > 0) {
+      getData();
+    }
+  }, [year]);
+
   return (
     <Layout title="RBYE">
       <div className="lg:max-w-6xl sm:m-auto">
         {/* <div className="lg:mx-40 sm:m-auto"> */}
-        <h6 className="text-right text-gray-500">
-          데이터 업데이트 {props.updated[0]} 데이터 수 {props.data.length}
-        </h6>
+        <div className="flex justify-between">
+          <h6 className="cursor-pointer inline-block text-right-left">
+            <span className="m-1" onClick={() => setYear(1)}>
+              [1년]
+            </span>
+            <span className="m-1" onClick={() => setYear(2)}>
+              [2년]
+            </span>
+            <span className="m-1" onClick={() => setYear(3)}>
+              [3년]
+            </span>
+            <span className="m-1" onClick={() => setYear(0)}>
+              [원래대로]
+            </span>
+          </h6>
+          <h6 className="text-right text-gray-500 inline-block">
+            데이터 업데이트 {props.updated[0]} 데이터 수 {props.data.length}
+          </h6>
+        </div>
         {/* <div className="lg:mx-auto lg:mx-32"> */}
         <div>
-          {props.data.map((job: Job) => (
+          {data.map((job: Job) => (
             <Jobs key={job.no} {...job} />
           ))}
+          {/* {props.data.map((job: Job) => (
+            <Jobs key={job.no} {...job} />
+          ))} */}
         </div>
       </div>
     </Layout>
