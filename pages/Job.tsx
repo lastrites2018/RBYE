be63dbf5b044a8d0acbed4693f6325776fd0c8a2
lastrites2018 <lastrites@nate.com>
2020-03-04 09:31:57 +0900

    스타일을 변경하고 근무지를 추가하라

--- pages/Job.tsx
+++ pages/Job.tsx
@@ -1,10 +1,18 @@
 import Markdown from "react-markdown";
+import { Job } from "../interfaces/index";
 
-const Jobs = ({ subject, companyName, contentObj, link, closingDate }) => (
+const Jobs = ({
+  subject,
+  companyName,
+  contentObj,
+  link,
+  closingDate,
+  workingArea
+}: Job) => (
   <div className="p-4 shadow rounded bg-white mt-2 sm:p-2 sm:m-2">
-    <h1 className="text-green-500 text-3xl ">{companyName}</h1>
+    <h2 className="text-gray-700">{companyName}</h2>
     <p className="text-gray-800">{subject}</p>
-    <div className="text-red-800 sm:m-2 md:m-10">
+    <div className="text-gray-600 sm:m-2 md:m-10">
       <Markdown source={contentObj?.requirement.replace(/• /gi, "- • ")} />
     </div>
     <h6 className="sm:m-2 md:m-4">우대사항</h6>
@@ -14,6 +22,7 @@ const Jobs = ({ subject, companyName, contentObj, link, closingDate }) => (
     <h6 className="sm:m-2 md:m-4">주요업무</h6>
     <p className="text-gray-600 mb-2 sm:m-2 md:m-4">{contentObj?.mainTask}</p>
     <div className="text-right">
+      <p className="ml-2 text-gray-500 text-sm">근무지 {workingArea}</p>
       <a
         className="text-blue-600 hover:text-blue-400"
         href={link}
