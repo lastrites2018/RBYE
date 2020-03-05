a0dd6eb42fd2a5a6105e2ef80a43cf1e124e53b5
lastrites2018 <lastrites@nate.com>
2020-03-05 22:11:01 +0900

    다중 공백을 하나의 공백으로 치환하라

--- components/Job.tsx
+++ components/Job.tsx
@@ -8,54 +8,58 @@ const Jobs = ({
   closingDate,
   workingArea,
   searchKeyword
-}: Job) => (
-  <div className="p-5 shadow rounded bg-white mt-3 sm:p-3 sm:m-3">
-    <h2 className="text-gray-700">
-      {" "}
-      <HighLight content={companyName} searchText={searchKeyword} />
-    </h2>
-    <p className="text-gray-800">
-      {" "}
-      <HighLight content={subject} searchText={searchKeyword} />
-    </p>
-    <div className="text-gray-600 sm:m-2 md:m-10 whitespace-pre-wrap">
-      <HighLight
-        content={
-          contentObj?.requirement &&
-          contentObj?.requirement
-            .replace(/\t/g, "") // 스타일 망가뜨리는 탭 문자 제거
-            .replace(/• |\* /gi, "- ")
-        }
-        searchText={searchKeyword}
-      />
-    </div>
-    <h6 className="sm:m-2 md:m-4">우대사항</h6>
-    <p className="text-gray-500 mb-2 sm:m-2 md:m-4">
-      <HighLight
-        content={contentObj?.preferentialTreatment}
-        searchText={searchKeyword}
-      />
-    </p>
-    <h6 className="sm:m-2 md:m-4">주요업무</h6>
-    <p className="text-gray-600 mb-2 sm:m-2 md:m-4">
-      <HighLight content={contentObj?.mainTask} searchText={searchKeyword} />
-    </p>
-    <div className="text-right">
-      <p className="ml-2 text-gray-500 text-sm">
-        근무지 <HighLight content={workingArea} searchText={searchKeyword} />
+}: Job) => {
+  const applyMultipleBlankToOneBlank = string => string.replace(/  +/g, " ");
+
+  let requirement =
+    contentObj.requirement &&
+    contentObj?.requirement
+      .replace(/\t/g, "") // 스타일 망가뜨리는 탭 문자 제거
+      .replace(/• |\* /gi, "- ");
+
+  requirement = applyMultipleBlankToOneBlank(requirement); // 스타일 망가뜨리는 다중 공백 제거
+
+  return (
+    <div className="p-5 shadow rounded bg-white mt-3 sm:p-3 sm:m-3">
+      <h2 className="text-gray-700">
+        {" "}
+        <HighLight content={companyName} searchText={searchKeyword} />
+      </h2>
+      <p className="text-gray-800">
+        {" "}
+        <HighLight content={subject} searchText={searchKeyword} />
       </p>
-      <a
-        className="text-blue-600 hover:text-blue-400"
-        href={link}
-        target="blank"
-      >
-        <HighLight content={link} searchText={searchKeyword} />
-      </a>
-      <span className="ml-2 text-gray-500">
-        마감일 <HighLight content={closingDate} searchText={searchKeyword} />
-      </span>
+      <div className="text-gray-600 sm:m-2 md:m-10 whitespace-pre-wrap">
+        <HighLight content={requirement} searchText={searchKeyword} />
+      </div>
+      <h6 className="sm:m-2 md:m-4">우대사항</h6>
+      <p className="text-gray-500 mb-2 sm:m-2 md:m-4">
+        <HighLight
+          content={contentObj?.preferentialTreatment}
+          searchText={searchKeyword}
+        />
+      </p>
+      <h6 className="sm:m-2 md:m-4">주요업무</h6>
+      <p className="text-gray-600 mb-2 sm:m-2 md:m-4">
+        <HighLight content={contentObj?.mainTask} searchText={searchKeyword} />
+      </p>
+      <div className="text-right">
+        <p className="ml-2 text-gray-500 text-sm">
+          근무지 <HighLight content={workingArea} searchText={searchKeyword} />
+        </p>
+        <a
+          className="text-blue-600 hover:text-blue-400"
+          href={link}
+          target="blank"
+        >
+          <HighLight content={link} searchText={searchKeyword} />
+        </a>
+        <span className="ml-2 text-gray-500">
+          마감일 <HighLight content={closingDate} searchText={searchKeyword} />
+        </span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Jobs;
