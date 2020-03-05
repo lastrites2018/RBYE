b0cabbd6638558b8d3aee4fe96afeb7c53eb7bc4
lastrites2018 <lastrites@nate.com>
2020-03-05 17:14:10 +0900

    하이라이트 관련 오류 메세지를 해결하기 위해 수정하라

--- components/HighLight.tsx
+++ components/HighLight.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import Highlighter from "react-highlight-words";
 
 export default function HighLight({ searchText, content }) {
+  if (!searchText) {
+    return <>{content}</>;
+  }
+
   return (
     <>
       <Highlighter
         highlightStyle={{ backgroundColor: "pink", padding: 0 }}
-        searchWords={[searchText]}
+        searchWords={[searchText && searchText]}
         autoEscape={true}
-        textToHighlight={content}
+        textToHighlight={content && content.toString()}
       />
     </>
   );
