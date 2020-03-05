48b4855f32ee2b9502d1bdb37e8347c8edc5e5cc
lastrites2018 <lastrites@nate.com>
2020-03-05 17:36:31 +0900

    footer에 안내 문구를 추가하라

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,12 +17,16 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div>
+      <div className="break-word-and-keep-all">
         이 사이트는{" "}
         <a href="/www.wanted.co.kr/" target="_blank">
           Wanted
         </a>
         의 데이터에 기반하며, 비영리적인 목적으로 사용합니다.
+        <p>
+          데이터는 비정기적으로 업데이트 되기에 최신 데이터가 아닐 수 있습니다.
+        </p>
+        <p>구직의 목적이라면 직접 원티드 사이트를 방문하시길 권장합니다.</p>
       </div>
     </footer>
   );
